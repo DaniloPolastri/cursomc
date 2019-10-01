e1307fcff76c5b929dc8589c7518d353615a6f84
Danilo <danilopolastri.ti@gmail.com>
2019-10-01 15:18:49 -0300

    FindAll da lista no /categorias e /clientes

--- src/main/java/com/nelioalves/cursomc/resources/CategoriaResources.java
+++ src/main/java/com/nelioalves/cursomc/resources/CategoriaResources.java
@@ -1,13 +1,18 @@
 package com.nelioalves.cursomc.resources;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.nelioalves.cursomc.domain.Categoria;
+import com.nelioalves.cursomc.domain.Cliente;
+import com.nelioalves.cursomc.repositories.CategoriaRepository;
 import com.nelioalves.cursomc.services.CategoriaService;
 
 @RestController
@@ -16,11 +21,22 @@ public class CategoriaResources {
 
 	@Autowired
 	private CategoriaService service;
-
+	
+	@Autowired
+	private CategoriaRepository cr;
+	
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
 	public ResponseEntity<?> find(@PathVariable Integer id) {
 		Categoria obj = service.buscar(id);
 		return ResponseEntity.ok().body(obj);
 
 	}
+	
+	
+	@RequestMapping
+	public List<Categoria> findAll(){
+		List<Categoria> categoria = service.Listar();
+		return categoria;
+	}
+	
 }
