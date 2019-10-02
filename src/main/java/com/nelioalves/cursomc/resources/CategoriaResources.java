1f547921f7615ce504bf7cff0953f8c6111d0e40
Danilo <danilopolastri.ti@gmail.com>
2019-10-02 14:47:18 -0300

    ItemPedido e ItemPedidoPK

--- src/main/java/com/nelioalves/cursomc/resources/CategoriaResources.java
+++ src/main/java/com/nelioalves/cursomc/resources/CategoriaResources.java
@@ -4,15 +4,12 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.nelioalves.cursomc.domain.Categoria;
-import com.nelioalves.cursomc.domain.Cliente;
-import com.nelioalves.cursomc.repositories.CategoriaRepository;
 import com.nelioalves.cursomc.services.CategoriaService;
 
 @RestController
@@ -22,8 +19,7 @@ public class CategoriaResources {
 	@Autowired
 	private CategoriaService service;
 	
-	@Autowired
-	private CategoriaRepository cr;
+	
 	
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
 	public ResponseEntity<?> find(@PathVariable Integer id) {
--- src/main/java/com/nelioalves/cursomc/resources/ClienteResources.java
+++ src/main/java/com/nelioalves/cursomc/resources/ClienteResources.java
@@ -4,14 +4,12 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.nelioalves.cursomc.domain.Cliente;
-import com.nelioalves.cursomc.repositories.ClienteRepository;
 import com.nelioalves.cursomc.services.ClienteService;
 
 @RestController
@@ -20,8 +18,7 @@ public class ClienteResources {
 
 	@Autowired
 	private ClienteService service;
-	@Autowired
-	private ClienteRepository cr;
+
 	
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
 	public ResponseEntity<?> find(@PathVariable Integer id) {
