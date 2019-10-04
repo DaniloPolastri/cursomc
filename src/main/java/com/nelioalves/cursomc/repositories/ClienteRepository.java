f6d738acbaaf70e6a9e4805f336ad4e0ae508a15
Danilo <danilopolastri.ti@gmail.com>
2019-10-04 13:40:43 -0300

    Email nao repetido na insercao de cliente

--- src/main/java/com/nelioalves/cursomc/repositories/ClienteRepository.java
+++ src/main/java/com/nelioalves/cursomc/repositories/ClienteRepository.java
@@ -1,11 +1,17 @@
 package com.nelioalves.cursomc.repositories;
 
+
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.nelioalves.cursomc.domain.Cliente;
 
 @Repository
 public interface ClienteRepository  extends JpaRepository<Cliente,Integer>{
 	
+	//faz fica mais rapida e diminui lock dentro do banco de dados
+	@Transactional(readOnly = true)
+	Cliente findByEmail(String email);
 }
