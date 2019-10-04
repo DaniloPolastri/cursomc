169a84f4c5b170819ff170471e9f70a03d844c44
Danilo <danilopolastri.ti@gmail.com>
2019-10-04 16:33:49 -0300

    Busca de produtos por nome e categorias

--- src/main/java/com/nelioalves/cursomc/services/PedidoService.java
+++ src/main/java/com/nelioalves/cursomc/services/PedidoService.java
@@ -1,6 +1,5 @@
 package com.nelioalves.cursomc.services;
 
-import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,7 +22,5 @@ public class PedidoService {
 	}
 	
 	
-	public List<Pedido> Listar(){
-		return repo.findAll();
-	}
+	
 }
